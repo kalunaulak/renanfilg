20d1e9ea212861ec34f74c2c1676bc97ad4e3365
kalunaulak <kaluancout@gmail.com>
2026-05-09 01:19:50 -0300

    fix: simplify build config to fix vercel deploy

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
-// https://vite.dev/config/
+// CONFIGURAÇÃO STANDARD PARA VERCEL
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    viteSingleFile(),
   ],
   build: {
     target: 'esnext',
-    assetsInlineLimit: 100000000, // Força o inlining de tudo
-    chunkSizeWarningLimit: 100000,
+    outDir: 'dist',
   },
 })
