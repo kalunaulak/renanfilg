b7f1a8acdf9208c1eb31e28400a591e1bb2eb76b
kalunaulak <kaluancout@gmail.com>
2026-05-09 01:23:28 -0300

    fix: update supabase api key

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://xugpssstgcinxzcqxbzz.supabase.co'
-const supabaseAnonKey = 'sb_publishable_yluedbhhw8wmk2hsqsbgtgnfx2sqxbzz'
+const supabaseAnonKey = 'sb_publishable_yIuedbhhw8wmk2Hb-Qjeog_7n17Jsr4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
